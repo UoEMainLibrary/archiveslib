6090066b8cfe22aa85b5c1ee8a2da264639c26f6
lacddt <lacddt@lac-liblabs-live2.is.ed.ac.uk>
2026-01-15 18:23:17 +0000

    removing accidental add

--- mgt_config/at_mysql.php
+++
@@ -1,5 +0,0 @@
-<? $hostname = 'lac-luna-dev1.is.ed.ac.uk';
-$username = 'arch';
-$password = '1m@stlbn';
-$dbname= 'archtoolkit';
-$id_link = mysql_connect($hostname, $username, $password);  ?>
--- mgt_config/at_mysql2.php
+++
@@ -1,5 +0,0 @@
-<? $hostname = 'lac-luna-dev1.is.ed.ac.uk';
-$username = 'arch';
-$password = '1m@stlbn';
-$dbname2= 'archtoolkit';
-$id_link2 = mysql_connect($hostname, $username, $password, true);  ?>
